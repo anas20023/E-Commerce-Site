09ee4d0f6105138134d7eda20bdf74d9f9c003d0
anas20023 <yorder07@gmail.com>
2023-12-11 09:03:38 +0600

    fixing file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
