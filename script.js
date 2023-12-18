7ea0df44ff9f079e4981c70412dab41a4f934a90
anas20023 <yorder07@gmail.com>
2023-12-18 18:42:43 +0600

    Update HTML, CSS, and JS files for newsletter popup and mobile navigation. (generated by blackbox.ai )

--- script.js
+++ script.js
@@ -5,18 +5,25 @@ let bg_newsltr=document.querySelector("#newsltr_bg");
 let main_newsltr_div=document.querySelector("#newsltr_main");
 let click_sm_nav=document.querySelector("#nav_sm");
 let show_sm_nav=document.querySelector("#hidden_nav");
+let cross_sm_nav=document.querySelector("#cross_nav");
 
 // For Newsletter
 
 nwesltr_close.addEventListener("click",()=>
 {
-    bg_newsltr.classList.add("hidden");
-    main_newsltr_div.classList.remove("flex");
-    main_newsltr_div.classList.add("hidden");
+    bg_newsltr.classList.add("-top-full");
+    main_newsltr_div.classList.remove("top-1/2");
+    main_newsltr_div.classList.add("-top-full");
 });
 
 // For Sm NavBar
 click_sm_nav.addEventListener("click",()=>{
     show_sm_nav.classList.remove("-left-full");
     show_sm_nav.classList.add("left-0");
-})
+});
+cross_sm_nav.addEventListener("click",()=>
+{
+    show_sm_nav.classList.remove("left-0");
+    show_sm_nav.classList.add("-left-full");
+
+});
