e80bb0cca89258f13bcc626a1dfef1a77e42a6b6
anas20023 <yorder07@gmail.com>
2023-12-20 04:06:01 +0600

    Update HTML and CSS files

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 /// All Node Variables ///
 
-let nwesltr_close=document.querySelector("#cross_newsltr");
-let bg_newsltr=document.querySelector("#newsltr_bg");
-let main_newsltr_div=document.querySelector("#newsltr_main");
-let click_sm_nav=document.querySelector("#nav_sm");
-let show_sm_nav=document.querySelector("#hidden_nav");
-let cross_sm_nav=document.querySelector("#cross_nav");
+let nwesltr_close = document.querySelector("#cross_newsltr");
+let bg_newsltr = document.querySelector("#newsltr_bg");
+let main_newsltr_div = document.querySelector("#newsltr_main");
+let click_sm_nav = document.querySelector("#nav_sm");
+let show_sm_nav = document.querySelector("#hidden_nav");
+let cross_sm_nav = document.querySelector("#cross_nav");
 
 // For Newsletter
 
-nwesltr_close.addEventListener("click",()=>
-{
-    bg_newsltr.classList.add("hidden");
-    main_newsltr_div.classList.remove("flex");
-    main_newsltr_div.classList.add("hidden");
+nwesltr_close.addEventListener("click", () => {
+  bg_newsltr.classList.add("hidden");
+  main_newsltr_div.classList.remove("flex");
+  main_newsltr_div.classList.add("hidden");
+  document.body.classList.remove("overflow-y-hidden");
 });
 
 // For Sm NavBar
-click_sm_nav.addEventListener("click",()=>{
-    show_sm_nav.classList.remove("-left-full");
-    show_sm_nav.classList.add("left-0");
+click_sm_nav.addEventListener("click", () => {
+  show_sm_nav.classList.remove("-left-full");
+  show_sm_nav.classList.add("left-0");
+  document.body.classList.add("overflow-y-hidden");
 });
-cross_sm_nav.addEventListener("click",()=>
-{
-    show_sm_nav.classList.remove("left-0");
-    show_sm_nav.classList.add("-left-full");
-
+cross_sm_nav.addEventListener("click", () => {
+  show_sm_nav.classList.remove("left-0");
+  show_sm_nav.classList.add("-left-full");
+  document.body.classList.remove("overflow-y-hidden");
 });
