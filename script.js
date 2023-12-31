bc93789dc92618750c1eb803f406efc573318246
anas20023 <yorder07@gmail.com>
2023-12-31 11:28:05 +0600

    Fix typo in minutes variable name

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let show_sm_nav = document.querySelector('#hidden_nav');
 let cross_sm_nav = document.querySelector('#cross_nav');
 let days = document.querySelector('#days');
 let hours = document.querySelector('#hrs');
-let mins = document.querySelector('#mins');
+let mints = document.querySelector('#mins');
 let secs = document.querySelector('#secs');
 
 // For Newsletter
@@ -33,3 +33,31 @@ cross_sm_nav.addEventListener('click', () => {
 });
 
 /// For Count Down of Banner Section
+
+const targetDate = new Date('December 31, 2023 23:59:59').getTime();
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const currentDate = new Date().getTime();
+
+  const remainingTime = targetDate - currentDate;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  days.innerHTML = `${days}`;
+  hours.innerHTML = `${hours}`;
+  mints.innerHTML = `${minutes}`;
+  secs.innerHTML = `${seconds}`;
+
+  // If the countdown is over, clear the interval
+  if (remainingTime < 0) {
+    clearInterval(countdownInterval);
+    countdownElement.innerHTML = 'Countdown expired!';
+  }
+}
